f1b6377396928cfc2c3aeed245f2656e95435b6e
Tabunov Egor <Vonubat@gmail.com>
2022-11-08 15:04:10 +0300

    feat: connect to MongoDB fix: change level structure

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,18 @@ import { PORT } from './constants';
 
 import * as serverService from './services/server.service';
 
-
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect(
+      'mongodb+srv://PMS-2022Q3:eskiIO8rHQanS92N@cluster-project-managem.3dng4tz.mongodb.net/managerApp'
+    );
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
-    })
+    });
   } catch (error) {
     console.log(error);
   }
 })();
-
-
 
 process.on('SIGINT', async () => {
   await mongoose.disconnect();
